72d9a686b4560dd468a35806989d3925dccdfa60
MarayahAy <83350135+MarayahAy@users.noreply.github.com>
2021-05-11 18:19:14 -0400

    first steps

--- Recipes Project.py
+++ Recipes Project.py
@@ -1,2 +1,23 @@
-x = 2+2
-print (x)
+#asking for available ingredients"""
+ingredients = input("what ingredients do you have available? (please seperate each with a comma)")
+print ('okay let me check what recipes we have available with', ingredients, '...')
+
+
+#importing recipes and store in dict varibale
+import csv
+recipes = csv.DictReader(open('recipes.csv'))
+for row in recipes:
+    print(row)
+
+def recipes(documentation):
+""" Description of the function
+Input:
+Ingredients that are available to user
+
+Output:
+All recipes that require only those ingredients
+
+"""
+    return documentation
+
+
