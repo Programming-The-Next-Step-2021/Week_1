46630ee16604def1728786b21c6d0d1f2ef8f827
MarayahAy <83350135+MarayahAy@users.noreply.github.com>
2021-05-19 10:04:59 -0400

    Update Recipes Project.py

--- Recipes Project.py
+++ Recipes Project.py
@@ -1,23 +1,36 @@
-#asking for available ingredients"""
-ingredients = input("what ingredients do you have available? (please seperate each with a comma)")
-print ('okay let me check what recipes we have available with', ingredients, '...')
+import spoonacular as sp
+api = sp.API("15f1634e59b646a2b48daa73b56b86af")
+
+# Search recipe by ingredients
+#all api functions found at link below
+# https://spoonacular.com/food-api/docs
+# Todo: add argument & choice to not have pantry items
+ingredients = input(__prompt= 'What are the ingredients you'd like to use?')
+response = api.search_recipes_by_ingredients(ingredients, ranking=2)
+data = response.json()
+recipeList = []
+recipeID = []
+
+#Todo: store the name of each recipe in choice list
+for i in range(len(data)):
+    recipeList += [data[i]['title']]
+    recipeID += [data[i]['id']]
+
+#display the recipes list
+choice = 1
+for entry in recipeList:
+    print(str(choice) +'. ' + entry)
+    choice += 1
+
+chosen = input('Please enter the number of the recipe you wish to view the directions for: \n')
+chosen = int(chosen)-1
+recipeChoice = recipeID[chosen]
+#recipeChoice = data[chosen]['id']
+
+moreresponse = api.get_recipe_information(recipeChoice)
+moredata = moreresponse.json()
+print(moredata['sourceUrl'])
 
 
-#importing recipes and store in dict varibale
-import csv
-recipes = csv.DictReader(open('recipes.csv'))
-for row in recipes:
-    print(row)
-
-def recipes(documentation):
-""" Description of the function
-Input:
-Ingredients that are available to user
-
-Output:
-All recipes that require only those ingredients
-
-"""
-    return documentation
 
 
