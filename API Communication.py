1669a3013e0e04620d6907d151d804d98f4fe6a0
MarayahAy <83350135+MarayahAy@users.noreply.github.com>
2021-05-19 10:38:50 -0400

    Update API Communication.py

--- API Communication.py
+++ API Communication.py
@@ -5,17 +5,39 @@ api = sp.API("15f1634e59b646a2b48daa73b56b86af")
 #all api functions found at link below
 # https://spoonacular.com/food-api/docs
 # Todo: add argument & choice to not have pantry items
-ingredients = input(__prompt= 'What are the ingredients you'd like to use?')
-response = api.search_recipes_by_ingredients(ingredients, ranking=2)
-data = response.json()
-recipeList = []
-recipeID = []
 
+def get_ingredients():
+    ingredients = input("what ingredients do you have available? (please seperate each with a comma)")
+    print ('okay let me check what recipes we have available with', ingredients, '...')
+#Todo: add error message for incorrect input
+
+def find_recipe():
+
+    '''
+    In this function the recipes in the Spoonacular Database
+    with the input ingredients are found and stored in a list.
+
+    recipeList is the list of all the suiting recipes.
+    recipeID is the ID number associated with the recipe from the databank.
+    '''
+
+    response = api.search_recipes_by_ingredients(ingredients, ranking=2)
+    data = response.json()
+    recipeList = []
+    recipeID = []
 #Todo: store the name of each recipe in choice list
+
 for i in range(len(data)):
     recipeList += [data[i]['title']]
     recipeID += [data[i]['id']]
 
+def choose_recipe():
+    '''
+    In this fuction the user can input the number associated with the recipe theu wish to view the directions for.
+    The fucntion will then redirect them to the chosen recipe.
+    :return:
+    '''
+#todo: make the recipe link open after choice
 #display the recipes list
 choice = 1
 for entry in recipeList:
