078041f77ae17519963f76a75b24b73b08f1a1b8
MarayahAy <83350135+MarayahAy@users.noreply.github.com>
2021-05-25 16:10:54 -0400

    Create test_Recipe_Finder.py

---
+++ Test/test_Recipe_Finder.py
@@ -0,0 +1,8 @@
+import unittest
+import Recipe_Finder
+
+class TestRecipe_Finder(unittest.TestCase):
+
+    def test_find_recipes(self):
+        api.search_recipes_by_ingredients(apple, ranking 2)
+
