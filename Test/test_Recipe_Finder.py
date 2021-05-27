0d998dce63945cf09cb80a75decbc344d2c70ae8
MarayahAy <83350135+MarayahAy@users.noreply.github.com>
2021-05-27 00:15:53 -0400

    Update test_Recipe_Finder.py

--- Test/test_Recipe_Finder.py
+++ Test/test_Recipe_Finder.py
@@ -1,8 +1,15 @@
 import unittest
-import Recipe_Finder
+from Recipe_Finder import find_recipes, recipe_link
 
-class TestRecipe_Finder(unittest.TestCase):
-
+class TestRecipeFinder(unittest.TestCase):
     def test_find_recipes(self):
-        api.search_recipes_by_ingredients(apple, ranking 2)
+        recipes = find_recipes('eggs,bacon,cheese')
+        self.assertIsNotNone(recipes,'no recipes')
 
+    def test_recipe_link(self):
+        link = recipe_link('716429')
+        self.assertEqual(self, link, 'https://fullbellysisters.blogspot.com/2012/06/pasta-with-garlic-scallions-cauliflower.html' )
+
+
+if __name__ == '__main__':
+    unittest.main()
